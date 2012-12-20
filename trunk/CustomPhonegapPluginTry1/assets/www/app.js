85ac9b06332c91713bbeb0388934660e7fb7f48e
jsphkhan <jsphkhan@e08c6aed-25f1-734a-5b5f-2566c136279d>
2012-12-20 17:58:08 +0000

    Checking network connection seems to be working now. Cordova 2.2.0 has a bug for navigator.connection.type Instead use navigator.network.connection.type

--- trunk/CustomPhonegapPluginTry1/assets/www/app.js
+++ trunk/CustomPhonegapPluginTry1/assets/www/app.js
@@ -13,8 +13,24 @@ function init(){
 	
 	//document.addEventListener("backbutton", onBackKeyDown, false);
 	//document.addEventListener("offline", onOffline, false);
-	//document.addEventListener("online", onOnline, false);	  
+	//document.addEventListener("online", onOnline, false);	 
+	//checkConnection(); 
 }
+
+function checkConnection(){
+	var networkState = navigator.network.connection.type;//navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
 // Handle the offline event
 //
 function onOffline() {
